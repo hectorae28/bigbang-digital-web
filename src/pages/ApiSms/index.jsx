cca64cd31715529c1068f7d146d00b564c9c15cc
Hector Archer <hectorae28@gmail.com>
2023-01-20 11:06:21 -0400

    fix url

--- src/pages/ApiSms/index.jsx
+++ src/pages/ApiSms/index.jsx
@@ -26,7 +26,7 @@ const ApiSms = () => {
       body: raw,
     };
 
-    fetch('throbbing-morning-3272.fly.dev', options)
+    fetch('http://throbbing-morning-3272.fly.dev', options)
       .then((response) => response.json())
       .then((response) => {
         setLoading(false);
