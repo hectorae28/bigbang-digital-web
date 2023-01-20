3bdb62ef1ab1316c3ee2110a77efe89ca754fa43
Hector Archer <hectorae28@gmail.com>
2023-01-20 16:29:44 -0400

    add firestore to contactMessage

--- src/pages/Contacts/index.jsx
+++ src/pages/Contacts/index.jsx
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { useRef,useState } from 'react';
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+import app from '../../firebase';
 import './styles.css';
 
 const Contact = () => {
+  const formContact = useRef(null)
+  const [errorMessage, setErrorMessage] = useState('')
+  //sorry for my ugly code
+  const handleContactSubmit = async (e) => {
+    e.preventDefault();
+    const formContactData = new FormData(formContact.current)
+    const db = getFirestore(app);
+    const raw = {
+      'name': formContactData.get('name'),
+      'last_name': formContactData.get('last_name'),
+      'phone': formContactData.get('phone'),
+      'email': formContactData.get('email'),
+      'message': formContactData.get('message'),
+    }
+    try {
+      await addDoc(collection(db, "messages"), raw);
+    } catch (e) {
+      setErrorMessage(`Hubo un error al enviar su mensaje :${e}`);
+    }
+  }
+
   return (
     <div className="Contacts">
       <h2 className="title">Contactanos</h2>
-      <form action="" method="POST">
-        <input type="text" name="name" id="name" placeholder="Nombre" />
+      <form ref={formContact} onSubmit={handleContactSubmit}>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Nombre"
+        />
         <input
           type="text"
           name="last_name"
           id="last_name"
           placeholder="Apellido"
         />
-        <input type="phone" name="phone" id="phone" placeholder="Telefono" />
+        <input
+          type="phone"
+          name="phone"
+          id="phone"
+          placeholder="Telefono"
+        />
         <input
           type="email"
           name="email"
@@ -23,14 +56,18 @@ const Contact = () => {
         <textarea
           name="message"
           id="message"
-          cols="30"
+          cols="50"
           rows="10"
           placeholder="Mensaje"
         ></textarea>
-        <button type="submit" className="ContactButton">
+        <button
+          type="submit"
+          className="ContactButton"
+        >
           Contactanos
         </button>
       </form>
+      <p>{errorMessage}</p>
     </div>
   );
 };
